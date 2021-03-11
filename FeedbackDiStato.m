312401acb01a589bd725373b4cde69d81e810e31
leobartowski <francescodangelo95@gmail.com>
2021-03-11 19:52:11 +0100

    Concluso il feedback di stato

--- FeedbackDiStato.m
+++ FeedbackDiStato.m
@@ -6,13 +6,15 @@ rho = 1;
 
 A = [0 1 0; 0 -.875 -20; 0 0 -50];
 B = [0 0 50]';
-C = [1 0 0; 0 1 0; 0 0 1]; %scelta da noi
-D = [0 0 0]'; %scelta da noi
+C = [1 0 0]; %scelta da noi
+D = 0; %scelta da noi
+
 x0 = [1 1 1]';
 
 
 ss = ss(A,B,C,D);
 tf = tf(ss);
+tzero = tzero(ss);
 
 G = [1 0 0; 0 gamma*1 0];
 H = [0 0]';
