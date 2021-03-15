f89521b03db76f195831d678b68dcf7624ffd227
Mary <dellarossamariarosaria@gmail.com>
2021-03-15 19:13:43 +0100

    lavorando su setpoint Papaer aircraft

--- AircraftPaper.m
+++ AircraftPaper.m
@@ -4,7 +4,7 @@ clearvars
 A = [-0.0558 -0.9968 0.0802 0.0415; 0.5980 -0.1150 -0.0318 0; -3.0500 0.3880 -0.4650 0; 0 0.0805 1 0];
 B = [0.0729 0; -4.7500 0.00775; 0.153 0.143; 0 0];
 C = [1 0 0 0; 0 0 0 1];
-D = 0;
+D = [0 0; 0 0];
 
 x0 = [0 0 0 pi/2];
 
@@ -38,4 +38,8 @@ K = ss(A-B*Kc-Kf*C,Kf,-Kc,0); %Sistema con output feedback
 LOss = Ps * K;
 LOss = minreal(LOss);
 
-bodemag(L, LOss), legend('L', 'Loss');
+% bodemag(L, LOss), legend('L', 'Loss');
+
+inversa = [A B; C D] \ [0 0 0 0 1 1]';
+F = inversa(1:4);
+N = inversa(5:6);
